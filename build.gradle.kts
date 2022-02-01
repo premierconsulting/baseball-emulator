5f34637a5a31e7858249aaed88fa291543031c6b
Premier Consulting <65988715+premierconsulting@users.noreply.github.com>
2022-02-01 15:33:43 -0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    // KSP support
+    id("com.google.devtools.ksp") version "1.6.10-1.0.2"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+val fritz2Version = "0.14"
+
+//group = "my.fritz2.app"
+//version = "0.0.1-SNAPSHOT"
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser()
+    }.binaries.executable()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("dev.fritz2:core:$fritz2Version")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
+/**
+ * KSP support - start
+ */
+dependencies {
+    add("kspMetadata", "dev.fritz2:lenses-annotation-processor:$fritz2Version")
+}
+kotlin.sourceSets.commonMain { kotlin.srcDir("build/generated/ksp/commonMain/kotlin") }
+tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().all {
+    if (name != "kspKotlinMetadata") dependsOn("kspKotlinMetadata")
+}
+// needed to work on Apple Silicon. Should be fixed by 1.6.20 (https://youtrack.jetbrains.com/issue/KT-49109#focus=Comments-27-5259190.0-0)
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
+}
+/**
+ * KSP support - end
+ */
